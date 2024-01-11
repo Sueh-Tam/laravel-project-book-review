cd4df7301d9528f8d99bd9db068347d5a5ed3dbf
Matheus Vidal Nunes <matheusvidal2012@gmail.com>
2024-01-11 10:05:17 -0300

    Implement cache function and show book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -26,7 +26,7 @@ class Book extends Model
             ->orderBy('reviews_count','DESC');
     }
 
-    public function scopeHighesRated(Builder $query, $from = null, $to = null): Builder{
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder{
         return  $query->withAvg([
             'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
         ],'rating')
@@ -44,5 +44,29 @@ class Book extends Model
         }elseif( $from && $to){
             $query->whereBetween('created_at', [$from, $to]);
         }
+    }
+
+    public function scopePopularLastMonth(Builder $query): Builder | QueryBuilder{
+        return $query->popular(now()->subMonth(), now())
+        ->HighestRated(now()->subMonth(), now())
+        ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder | QueryBuilder{
+        return $query->popular(now()->subMonths(6), now())
+        ->HighestRated(now()->subMonths(6), now())
+        ->minReviews(5);
+    }
+
+    public function scopeHighestRatedPopularLastMonth(Builder $query): Builder | QueryBuilder{
+        return $query->popular(now()->subMonth(), now())
+        ->HighestRated(now()->subMonth(), now())
+        ->minReviews(2);
+    }
+
+    public function scopeHighestRatedPopularLast6Month(Builder $query): Builder | QueryBuilder{
+        return $query->popular(now()->subMonths(6), now())
+        ->HighestRated(now()->subMonths(6), now())
+        ->minReviews(2);
     }
 }
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="mb-10 text-2x1">
+        Books
+    </h1>
+    <form method="GET" action="{{Route('books.index')}}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" class="input h-10" placeholder="Search by title" value="{{request('title')}}">
+        <input type="hidden" value="{{request('filter')}}">
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{route('books.index')}}" class="btn h-10 ">Clear</a>
+    </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Popular Rated Last Month',
+                'highest_rated_last_6month' => 'Popular Rated Last 6 Months'
+            ];
+        @endphp
+        @foreach ($filters as $key => $label)
+            <a href="{{Route('books.index', [...request()->query(),'filter' => $key])}}" class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+                {{ $label }}
+            </a>
+        @endforeach
+    </div>
+        
+    <ul>
+        @forelse ($books as $book)
+            <li class="mb-4">
+                <div class="book-item">
+                    <div
+                    class="flex flex-wrap items-center justify-between">
+                    <div class="w-full flex-grow sm:w-auto">
+                        <a href="{{Route('books.show',$book)}}" class="book-title">{{$book->title}}</a>
+                        <span class="book-author">By {{$book->author}}</span>
+                    </div>
+                    <div>
+                        <div class="book-rating">
+                            {{number_format($book->reviews_avg_rating, 1)}}
+                        </div>
+                        <div class="book-review-count">
+                        out of {{$book->reviews_count}} {{Str::plural('review', 2)}}
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text"></p>
+                    <a href="{{ Route('books.index') }}" class="reset-link">Reset Criteria</a>
+                </div>
+            </li>
+        @endforelse
+    </ul>
+@endsection
